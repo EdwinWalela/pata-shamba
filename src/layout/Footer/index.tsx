38a40e59b8e3f6e80fb4de54133b3b84e3537aa8
edwinwalela <edwinwalela@gmail.com>
2023-08-08 16:57:03 +0300

    update footer

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -2,7 +2,8 @@ const Footer = () => {
 	return (
 		<footer className="p-4  shadow md:px-6 md:py-8 bg-gray-900">
 			<span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-				© 2022 <a href="https://edwinwalela.com/" target="_blank" className="hover:underline"></a>
+				PataShamba © 2023{' '}
+				<a href="https://edwinwalela.com/" target="_blank" className="hover:underline"></a>
 			</span>
 		</footer>
 	);
